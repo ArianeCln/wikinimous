fdc2c00349c7458a04924d22a609f12796d046a0
Ariane Coulon <ariane.coulon@gmail.com>
2016-02-09 16:55:00 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  @article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraphs,
+  )
+  @article.save
+end
